d4e2128273208468d08ec26de96f1f91144362b8
lecanardjaune <ugiraud@edenschool.fr>
2022-06-08 10:31:34 +0200

    evo

--- js/app.js
+++ js/app.js
@@ -2,9 +2,26 @@ const start = document.getElementById("start");
 const end = document.getElementById("end");
 const blocks = document.getElementsByClassName("block");
 
+// for(const block of blocks){
+//     block.addEventListener("click", () => {
+//         if(block.parentElement.id == "start"){
+//             end.appendChild(block);
+//         } else{
+//             start.appendChild(block)
+//         }
+        
+//     })
+// }
+
+// EVO ----------
+
 for(const block of blocks){
-    block.addEventListener("click", () => {
-            start.removeChild(block);
-            end.appendChild(block);     
-    })    
+    block.addEventListener("click", ()=>{
+        block.classList.add("move");
+        setTimeout(()=>{
+            start.removeChild(block)
+            block.classList.remove("move");
+            end.appendChild(block)
+        },1000)
+    })
 }
